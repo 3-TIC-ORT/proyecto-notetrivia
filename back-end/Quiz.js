141c0311e407dce52a80d08065264a3b1c38a744
BARCELO JOSE <50256765@ort>
2025-09-26 15:49:37 -0300

    empeze a modificar prompt y response

--- back-end/Quiz.js
+++ back-end/Quiz.js
@@ -4,8 +4,23 @@ import 'dotenv/config';
 const cohere = new CohereClientV2({
   token: process.env.API,
 });
+let input = `
+La Revolución Francesa (1789-1799) fue un proceso que, impulsado por crisis económica, desigualdad social e ideas ilustradas, derrocó a la monarquía, instauró la república, atravesó el Terror, terminó con el Directorio y dio paso al ascenso de Napoleón, logrando el fin del absolutismo y difundiendo ideales de libertad e igualdad.`
+const prompt = input + `: sos un generador de trivias adecuadas a este contenido. Tu objetivo es Genera una trivia de 5 preguntas con 4 opciones cada una en formato JSON. 
+La estructura debe ser:
+{
+  "trivia": [
+    {
+      "pregunta": "string",
+      "opciones": [
+        { "texto": "string", "correcta": true/false },
+        ...
+      ]
+    }
+  ]
+}
+IMPORTANTE: Devuelve únicamente el JSON, sin explicaciones ni formato extra.`;
 
-const prompt = "cuando nacio san martin";
 
 async function main() {
   try {
