6cabda71e91c3d5c763fcc2fe6fd68197d780a75
jose <josebarcelodawi@gmail.com>
2025-10-20 11:21:54 -0300

    mejore estructura de usuarios.json

--- back-end/server.js
+++ back-end/server.js
@@ -15,13 +15,22 @@ const io = new Server(server, {
 });
 
 app.use(express.static(path.join(__dirname, "..", "front-end")));
-
-
 let usuarios = JSON.parse(fs.readFileSync("usuarios.json"));
+let proximoid = Object.values(usuarios).reduce((max, u) => Math.max(max, u.id), 0) + 1;// devuele array de objetos lo recorre acumula y con max usa el mayor entre el acumulado y el id del usuario actual.
 let documento = "";
+ const guardarSALA = (room, contenido) => { // declaramos funciones para que sea mas facil despues
+   fs.writeFileSync("sala" + room + ".txt", contenido);
+ }
+  const cargarSALA = (room) => { 
+      if (fs.existsSync("sala" + room + ".txt")) {
+        return fs.readFileSync("sala" + room + ".txt", "utf-8");
+      } else {
+        return "";
+      }
+        }
+ let salasDATA = {}
 io.on("connection", (socket) => {
   console.log("papu conectado");
-
   socket.on("registro", (data) => {
     if (usuarios[data.user]) {
       socket.emit("registro-error", { mensaje: "El usuario ya existe" });
@@ -30,7 +39,12 @@ io.on("connection", (socket) => {
       socket.emit("registro-error", { mensaje: "La contraseña tiene que tener al menos 6 caracteres" });
       return;
     } else {
-      usuarios[data.user] = data.password;
+      usuarios[data.user] = {
+        id: proximoid,
+        password: data.password,
+        username: data.user
+      }
+      proximoid++;
       socket.emit("registro-exito", { mensaje: "Usuario registrado con éxito" });
       console.log(usuarios)
       fs.writeFileSync("usuarios.json", JSON.stringify(usuarios, null, 2));
@@ -40,19 +54,24 @@ io.on("connection", (socket) => {
     if (!usuarios[data.user]) {
       socket.emit("login-error", { mensaje: "El usuario no existe" });
       return;
-    } else if (usuarios[data.user] !== data.password) {
+    } else if (usuarios[data.user].password !== data.password) {
         socket.emit("login-error", { mensaje: "Contraseña incorrecta" });
         return;
     } else {
       socket.emit("login-exito", { mensaje: "Login exitoso" });
     }
+  });
+    socket.on("unirse-sala", (data) => {
+    const room = data.room;
+    socket.join(room);
+    cargarSALA(room);
+    if (!salasDATA[room]) {
+      salasDATA[room] = "";}
+    });
 });
-socket.emit("cargar", documento);
-    socket.on("editarDocumento", (data) => {
-      documento = data;
-      socket.broadcast.emit("actualizar", data);
-    });
-    });
+
+//                        edicio colaborativa
+
     
 server.listen(3000, () => {
   console.log("Servidor corriendo en http://localhost:3000");
